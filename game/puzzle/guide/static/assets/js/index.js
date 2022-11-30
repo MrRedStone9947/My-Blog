e429599bd71e487d6d76b1ff7c71bf2d4f980ea2
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-11-30 13:50:18 +0800

    1

---
+++ game/puzzle/guide/static/assets/js/index.js
@@ -0,0 +1,21 @@
+var yes=0,no=0;
+function get(){
+    var v=document.getElementById("in").value;
+    if(v=="rickroll"){
+        if(yes<=10){
+            alert("密码正确！……但是神魔都没有发生");
+        }
+        else{
+            alert("密码正确！……似乎有所不同");
+            document.getElementById("hint").innerHTML="下一关密码:11451411451454188<br>下一关提示:";
+        }
+        yes+=1;
+    }
+    else{
+        alert("你没逝吧");
+        no+=1;
+        if(no>=10){
+            window.location.replace('https://www.bilibili.com/video/BV1uT4y1P7CX')
+        }
+    }
+}
