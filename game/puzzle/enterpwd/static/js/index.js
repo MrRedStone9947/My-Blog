fd85e73bc41feab7d6e37541ccc1abef43c28685
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-11-30 18:02:56 +0800

    1

---
+++ game/puzzle/enterpwd/static/js/index.js
@@ -0,0 +1,5 @@
+var ans_list=[1]
+function enterPwd(index){
+    alert(ans_list[-1]);
+    alert(index)
+}
---
+++ game/puzzle/guide/static/js/index-original.js
@@ -0,0 +1,25 @@
+var yes=0,no=0;
+function get(){
+    var v=hex_md5(document.getElementById("in").value);
+    if(v=="667ff118ef6d196c96313aeaee7da519"){
+        if(yes<=10){
+            alert("密码正确！……但是神魔都没有发生");
+        }
+        else{
+            alert("密码正确！……似乎有所不同");
+            var ans1="下一关密码:114";
+            var ans2="5141145";
+            var ans3="1454188<b";
+            var ans4="r>下一关提示:"
+            document.getElementById("hint").innerHTML=ans1+ans2+ans3+ans4;
+        }
+        yes+=1;
+    }
+    else{
+        alert("你没逝吧，这都能输错，真的是牛波一");
+        no+=1;
+        if(no>=10){
+            window.location.replace('https://www.bilibili.com/video/BV1uT4y1P7CX')
+        }
+    }
+}
