51227cd1dc31829b1310898a15a7352745871ef8
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-11-30 15:34:45 +0800

    Update index.js

--- game/puzzle/guide/static/js/index.js
+++ game/puzzle/guide/static/js/index.js
@@ -1,7 +1,8 @@
+document.write("<script src="http://redstone.62148310.xyz/static/plugins/md5.js"></script>")
 var yes=0,no=0;
 function get(){
-    var v=document.getElementById("in").value;
-    if(v=="rickroll"){
+    var v=hex_md5(document.getElementById("in").value);
+    if(v=="667ff118ef6d196c96313aeaee7da519"){
         if(yes<=10){
             alert("密码正确！……但是神魔都没有发生");
         }
