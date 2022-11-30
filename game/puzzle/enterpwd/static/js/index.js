fffb81cbcb980eb400f443164e560b309298ced3
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-11-30 19:31:12 +0800

    Update index.js

--- game/puzzle/enterpwd/static/js/index.js
+++ game/puzzle/enterpwd/static/js/index.js
@@ -1,18 +1,16 @@
 var ans_list=['11451411451454188','WDNMDJNTM'];
 var href_list=['truelevels/level1','truelevels/level2'];
-var id=-1;
-function get(){
-    alert(id);
-    var v=document.getElementById("in").value;
-    if(v==ans_list[id-1]){
-        window.location.href='http://redstone.62148310.xyz/game/puzzle/'+href_list[id-1];
-    }
-    else{
-        alert("恭喜你，密码错了!")
-        window.location.href='http://redstone.62148310.xyz';
-    }
-}
 function enterPwd(index){
     window.location.href='http://redstone.62148310.xyz/game/puzzle/enterpwd';
-    id=index;
+    function get(){
+        alert(index);
+        var v=document.getElementById("in").value;
+        if(v==ans_list[index-1]){
+            window.location.href='http://redstone.62148310.xyz/game/puzzle/'+href_list[index-1];
+        }
+        else{
+            alert("恭喜你，密码错了!")
+            window.location.href='http://redstone.62148310.xyz';
+        }
+    }
 }
