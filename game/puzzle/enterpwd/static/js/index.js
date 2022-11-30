bb04970011bca783e44d4ce6dd336653fa567f9a
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-11-30 19:14:44 +0800

    Update index.js

--- game/puzzle/enterpwd/static/js/index.js
+++ game/puzzle/enterpwd/static/js/index.js
@@ -2,6 +2,7 @@ var ans_list=['11451411451454188','WDNMDJNTM'];
 var href_list=['truelevels/level1','truelevels/level2'];
 var id=-1;
 function get(){
+    alert(id);
     var v=document.getElementById("in").value;
     if(v==ans_list[id-1]){
         window.location.href='http://redstone.62148310.xyz/game/puzzle/'+href_list[id-1];
