842855bee76575fcbff06656fa4f33fce9df2e62
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-11-30 15:56:34 +0800

    Update index.js

--- game/puzzle/guide/static/js/index.js
+++ game/puzzle/guide/static/js/index.js
@@ -1,6 +1,7 @@
 var yes=0,no=0;
 function get(){
     var v=hex_md5(document.getElementById("in").value);
+    alert(v);
     if(v=="667ff118ef6d196c96313aeaee7da519"){
         if(yes<=10){
             alert("密码正确！……但是神魔都没有发生");
