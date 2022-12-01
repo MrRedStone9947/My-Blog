dc0c6e13cabe8c6ae6bab6560d01c3a325c666ca
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-12-01 10:08:37 +0800

    Update index.js

--- game/puzzle/truelevels/level1/static/js/index.js
+++ game/puzzle/truelevels/level1/static/js/index.js
@@ -1,6 +1,7 @@
 var f=0;
 function get(){
     var v=hex_md5(document.getElementById('in').value);
+    console.log(v);
     if(v=='8139ca2f8d66'+'2f052a5b9eff9a344e7e'&&f==0){
         f=1;
         document.getElementById('title').innerHTML='恭喜你通过了前面的小关卡，现在来开始第一关吧！';
