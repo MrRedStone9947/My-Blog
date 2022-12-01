fcc50f068460370a01fdcc884c6e6e9fe04b8019
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-12-01 09:59:17 +0800

    1

--- game/puzzle/truelevels/level1/static/js/index.js
+++ game/puzzle/truelevels/level1/static/js/index.js
@@ -5,11 +5,13 @@ function get(){
     if(v=='8139ca2f8d66'+'2f052a5b9eff9a344e7e'&&f==0){
         f=1;
         document.getElementById('title').innerHTML='恭喜你通过了前面的小关卡，现在来开始第一关吧！';
+        document.getElementById('laugh').innerHTML='';
     }
     else if(v=='bcc2f57d3913ab43ccaa0'+'4d92503e9ba'&&f==1){
         document.getElementById('hint').innerHTML='asdasd';
+        document.getElementById('laugh').innerHTML='';
     }
     else{
-        document.getElementById('in').value='这都错了，你不会没做上一关吧:)';
+        document.getElementById('laugh').innerHTML='这都错了，您不会没做上一关吧！';
     }
 }
