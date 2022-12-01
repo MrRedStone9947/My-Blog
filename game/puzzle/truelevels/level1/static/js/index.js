eb198cee2e32ef9cb5f5dc001a837c54309ae346
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-12-01 10:04:48 +0800

    Update index.js

--- game/puzzle/truelevels/level1/static/js/index.js
+++ game/puzzle/truelevels/level1/static/js/index.js
@@ -1,6 +1,6 @@
 var f=0;
 function get(){
-    var v=hex_md5(document.getElementById('in'));
+    var v=document.getElementById('in');
     console.log(v);
     if(v=='8139ca2f8d66'+'2f052a5b9eff9a344e7e'&&f==0){
         f=1;
