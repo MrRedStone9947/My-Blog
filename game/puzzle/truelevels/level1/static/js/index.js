89031a044c16282995d40a4b8a380677875fb47b
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-12-01 10:39:54 +0800

    1

--- game/puzzle/truelevels/level1/static/js/index.js
+++ game/puzzle/truelevels/level1/static/js/index.js
@@ -1,7 +1,6 @@
 var f=0;
 function get(){
     var v=hex_md5(document.getElementById('in').value);
-    console.log(v);
     if(v=='4a4b95e23'+'784821549837819d97cbfe2'&&f==0){
         f=1;
         document.getElementById('title').innerHTML='恭喜你通过了前面的小关卡，现在来开始第一关吧！';
@@ -10,7 +9,7 @@ function get(){
         document.getElementById('t0').innerHTML='提示:找规律';
         document.getElementById('t1').innerHTML='AB+CD=WDNM';
         document.getElementById('t2').innerHTML='CD+EF=DJNTM';
-        document.getElementById('t3').innerHTML='(AB+2CD)/EF*AB*EF/AB=';
+        document.getElementById('t3').innerHTML='(AB+2CD)/EF*AB*EF/AB+2EF-EF=';
     }
     else if(v=='bcc2f57d3913ab43ccaa0'+'4d92503e9ba'&&f==1){
         document.getElementById('hint').innerHTML='asdasd';
