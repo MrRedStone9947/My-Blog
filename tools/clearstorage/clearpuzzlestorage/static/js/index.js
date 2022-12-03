a9acaf24a829054402acc1bbfff7b512fa386f0b
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-12-03 18:41:21 +0800

    1

--- tools/clearstorage/clearpuzzlestorage/static/js/index.js
+++ tools/clearstorage/clearpuzzlestorage/static/js/index.js
@@ -1,6 +1,6 @@
 function main(){
     console.log("FUCK");
-    if(document.getElementById('clear_level4').value==1){
+    document.getElementById('clear_level4').onclick=function(){
         alert("清除第4关缓存成功！");
         localStorage.removeItem("redstone.62148310.xyz/game/puzzle/truelevels/level4.vis");
     }
