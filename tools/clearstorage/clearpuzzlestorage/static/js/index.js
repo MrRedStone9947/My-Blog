673d0e41320e253815364b4450d3754a741321bd
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-12-03 17:57:26 +0800

    1

--- tools/clearstorage/clearpuzzlestorage/static/js/index.js
+++ tools/clearstorage/clearpuzzlestorage/static/js/index.js
@@ -1,8 +1,8 @@
-window.onunload=function(){
-    while(true){
-        if(document.getElementById('clear_level4')==1){
-            localStorage.removeItem("redstone.62148310.xyz/game/puzzle/truelevels/level4.vis");
-            console.log("清除第4关缓存成功！");
-        }
+function main(){
+    if(document.getElementById('clear_level4')==1){
+        localStorage.removeItem("redstone.62148310.xyz/game/puzzle/truelevels/level4.vis");
+        console.log("清除第4关缓存成功！");
     }
 }
+
+const loopId=setInterval(main,500);
