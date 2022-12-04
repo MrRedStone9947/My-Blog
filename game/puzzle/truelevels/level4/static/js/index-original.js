b6f4eb502f6f73b27e9f053ad16f5b2ae752be3a
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2022-12-04 10:53:44 +0800

    1

---
+++ game/puzzle/truelevels/level4/static/js/index-original.js
@@ -0,0 +1,38 @@
+var f=0;
+if(localStorage.getItem("redstone.62148310.xyz/game/puzzle/truelevels/level4.vis")==1){
+    window.location.replace('http://redstone.62148310.xyz');
+}
+localStorage.setItem("redstone.62148310.xyz/game/puzzle/truelevels/level4.vis",1);
+function jump(){
+    window.location.replace('https://www.bilibili.com/video/BV1uT4y1P7CX');
+}
+function get(){
+    var v=hex_md5(document.getElementById('in').value);
+    if(v=='7e875d74f951240406'+'46c0038384e14f'&&f==0){
+        f=1;
+        document.getElementById('title').innerHTML='好的请再输入前面某一关的秘钥';
+        document.getElementById('t1').innerHTML='本页面将于2分钟后过期，快点输入！！';
+        document.getElementById('t2').innerHTML='不要刷新，后果自负！';
+        document.getElementById('t3').innerHTML='不要关闭页面，后果自负！！';
+        document.getElementById('laugh').innerHTML='';
+        document.getElementById('in').value='';
+        setTimeout(jump,2*60*1000);
+    }
+    else if(v=='68a8a'+'7df567c08f6a8ff4f8efa8e1ae6'&&f==1){
+        var ans1='本关秘钥:Really';
+        var ans2='MyGood?<br';
+        var ans3='>下一关提示:tru';
+        var ans4='elevels/lev';
+        var ans5='el5';
+        document.getElementById('hint').innerHTML=ans1+ans2+ans3+ans4+ans5;
+        document.getElementById('laugh').innerHTML='';
+    }
+    else{
+        if(f==1){
+            document.getElementById('laugh').innerHTML='恭喜你，密钥可太正确了，都没对！！';
+        }
+        if(f==0){
+            document.getElementById('laugh').innerHTML='我真服了，这能输错，牛波一';
+        }
+    }
+}
--- tools/clearstorage/clearpuzzlestorage/static/js/index.js
+++ tools/clearstorage/clearpuzzlestorage/static/js/index.js
@@ -1,5 +1,4 @@
 function main(){
-    console.log("FUCK");
     document.getElementById('clear_level4').onclick=function(){
         alert("清除第4关缓存成功！");
         localStorage.removeItem("redstone.62148310.xyz/game/puzzle/truelevels/level4.vis");
